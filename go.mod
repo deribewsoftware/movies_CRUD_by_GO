7725598b63664bcb9f0bcb90693c4334cef51656
deribew <deribewsoftware@gmail.com>
2024-01-05 16:31:37 +0300

    updated

---
+++ go.mod
@@ -0,0 +1,5 @@
+module webapp/go_movies
+
+go 1.21.5
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -26,6 +26,56 @@ func getMovies(w http.ResponseWriter,r *http.Request){
 	json.NewEncoder(w).Encode(movies)
 }
 
+func deleteMovie(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params:=mux.Vars(r)
+	for index,item:= range movies{
+		if item.ID==params["id"]{
+       movies=append(movies[:index],movies[index+1:]...)
+			 break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params:=mux.Vars(r)
+	for _,item:= range movies{
+		if item.ID==params["id"]{
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+
+}
+
+//createMovie
+func createMovie(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_=json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID=strconv.Itoa(rand.Intn(100000000))
+	movies=append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter,r *http.Request){
+	//content type
+	w.Header().Set("Content-Type", "application/json")
+	params:=mux.Vars(r)
+	for index,item:=range movies{
+		if item.ID==params["id"]{
+			movies=append(movies[:index],movies[index+1:]...)
+			var movie Movie
+			_=json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID=params["id"]
+			movies=append(movies,movie)
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
+}
+
 func main(){
 r:=mux.NewRouter()
 
@@ -34,10 +84,11 @@ movies=append(movies, Movie{ID: "2", Isbn: "49355", Title: "Movie 2",Director:&D
 movies=append(movies, Movie{ID: "3", Isbn: "83332", Title: "movi 3",Director:&Director{FirstName: "Endale", LastName: "Shimelis"}})
 r.HandleFunc("/movies",getMovies).Methods("GET")
 r.HandleFunc("/movies/{id}",getMovie).Methods("GET")
-r.HandleFunc("/createMovie",createMovie).Methods("POST")
+r.HandleFunc("/create-movie",createMovie).Methods("POST")
 r.HandleFunc("/update/{id}",updateMovie).Methods("PUT")
 r.HandleFunc("/delete/{id}",deleteMovie).Methods("DELETE")
 fmt.Println("Starting Server at port :8000")
 log.Fatal(http.ListenAndServe(":8000", r))
 
 }
+
